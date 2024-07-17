7617d2d7a7bdd761236ece1fc7fcbc7a154c9e42
KravchenkoII <group777@mail.ru>
2024-07-18 00:27:54 +0300

    Практическое задание по уроку "Базовые структуры данных"

---
+++ main.py
@@ -0,0 +1,20 @@
+# 1st program
+print(5*9**0.5)
+
+# 2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3st program
+print((1234 % 1000)//10+(5678 % 1000)//10)
+
+# 4st program
+x=13.42
+x1=int(x)
+x2=int(round(x-x1,2)*100)
+print(x1, x2)
+
+y=42.13
+y1=int(y)
+y2=int(round(y-y1,2)*100)
+print(y1, y2)
+print(x1==y2 or x2==y1)
